1703cd464c4cfc8f09b96ae0ce38ac7c74d7ada8
Om Bhesania <bitcraftnetworks@gmail.com>
2025-08-02 01:36:41 +0530

    refactor: simplify Calendly component by removing unused props and updating data-url

--- src/components/Calendly.tsx
+++ src/components/Calendly.tsx
@@ -3,24 +3,25 @@ interface CalendlyWidgetProps {
   primaryColor?: string;
   textColor?: string;
 }
-const Calendly: React.FC<CalendlyWidgetProps> = ({
-  backgroundColor = "F8FFF4",
-  primaryColor = "f6985f",
-  textColor = "333333",
-}) => {
+const Calendly: React.FC<CalendlyWidgetProps> = (
+  {
+    // backgroundColor = "F8FFF4",
+    // primaryColor = "f6985f",
+    // textColor = "333333",
+  }
+) => {
   return (
     <>
       <div
         className="calendly-inline-widget"
-        data-url={`https://calendly.com/bitcraftnetworks/30min?hide_event_type_details=1&hide_gdpr_banner=1&background_color=${backgroundColor}&text_color=${textColor}&primary_color=${primaryColor}`}
+        data-url="https://calendly.com/docversemedia/30min?primary_color=676767"
         style={{ minWidth: "320px", height: "700px" }}
-      >
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async
-        ></script>
-      </div>
+      ></div>
+      <script
+        type="text/javascript"
+        src="https://assets.calendly.com/assets/external/widget.js"
+        async
+      ></script>
     </>
   );
 };
